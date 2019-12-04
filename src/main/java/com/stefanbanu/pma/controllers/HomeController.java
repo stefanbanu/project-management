6fe9cee83794701cd56a67d8bc1228eba28c7b15
Stefan Banu <stefanbanu1@gmail.com>
2019-12-04 06:32:05 +0200

    styled home.html page and added the pie chart from chart.js.

--- src/main/java/com/stefanbanu/pma/controllers/HomeController.java
+++ src/main/java/com/stefanbanu/pma/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.stefanbanu.pma.controllers;
 
+import com.stefanbanu.pma.dao.EmployeeRepository;
 import com.stefanbanu.pma.dao.ProjectRepository;
+import com.stefanbanu.pma.entities.Employee;
 import com.stefanbanu.pma.entities.Project;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +17,14 @@ import java.util.List;
 public class HomeController {
 
     private final ProjectRepository projectRepository;
+    private final EmployeeRepository employeeRepository;
 
     @GetMapping("/")
     public String displayHome(Model model) {
         List<Project> projects = projectRepository.findAll();
+        List<Employee> employees = employeeRepository.findAll();
         model.addAttribute("projects", projects);
+        model.addAttribute("employees", employees);
         return "main/home";
     }
 }
