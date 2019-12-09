3746aeef87dd0a01ad5adf6828ed388ff60c9cc5
Stefan Banu <stefanbanu1@gmail.com>
2019-12-09 03:31:31 +0200

    added integration test.

---
+++ src/test/java/com/jrp/dao/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.jrp.dao;
+
+import com.stefanbanu.pma.ProjectManagementApplication;
+import com.stefanbanu.pma.dao.ProjectRepository;
+import com.stefanbanu.pma.entities.Project;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@ContextConfiguration(classes = ProjectManagementApplication.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
+        @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")})
+        public class ProjectRepositoryIntegrationTest{
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Test
+    public void ifNewProjectSaved_Success() {
+        Project newProject = new Project("New Test Project", "COMPLETE", "Test Description");
+        projectRepository.save(newProject);
+
+
+        assertEquals(5, projectRepository.findAll().size());
+    }
+}
