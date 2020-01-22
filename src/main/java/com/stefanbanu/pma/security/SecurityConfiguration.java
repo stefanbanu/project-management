b7fa62d4c477dcb5f954f2e529a60565fcaca640
stefanbanu <stefanbanu1@gmail.com>
2020-01-22 03:28:37 +0200

    added different authorizations.

--- src/main/java/com/stefanbanu/pma/security/SecurityConfiguration.java
+++ src/main/java/com/stefanbanu/pma/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.stefanbanu.pma.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,12 +21,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("taz")
                     .password("pass2")
-                    .roles("USER");
+                    .roles("USER")
+                .and()
+                .withUser("managerUser")
+                .password("pass3")
+                .roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncode() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)throws Exception {
+        http.csrf().disable().httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/projects/new")
+                .hasRole("ADMIN")
+                .antMatchers("/projects/save")
+                .hasRole("ADMIN")
+                .antMatchers("/")
+                .authenticated();
+//                .and()
+//                .formLogin();
     }
 
 }
